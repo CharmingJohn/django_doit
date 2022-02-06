e36e43b07af352ccb70a5d29715c8b41fd885db9
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-02-06 16:20:15 +0900

    add Preview and summary

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import os
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_test = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -11,9 +13,16 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # author: tbd
 
     def __str__(self):
         return '[{}] {}'.format(self.pk, self.title)
 
     def get_absolute_url(self):
         return '/blog/{}'.format(self.pk)
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
