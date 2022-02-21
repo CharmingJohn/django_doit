6113ebdfc9169e2706ef8a62773cc495b6b1ff25
CharmingJohn <45421181+CharmingJohn@users.noreply.github.com>
2022-02-21 14:24:53 +0900

    leave comment on post detail page with comment form

--- blog/tests.py
+++ blog/tests.py
@@ -122,6 +122,54 @@ class TestView(TestCase):
         self.assertIn('Hi, we are the world', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
 
+
+    def test_comment_form(self):
+        self.assertEqual(Comment.objects.count(), 1)
+        self.assertEqual(self.post_001.comment_set.count(), 1)
+
+        # status log out
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment-area')
+        self.assertIn('Log in and leave a comment', comment_area.text)
+        self.assertFalse(comment_area.find('form', id='comment-form'))
+
+        # status log in
+        self.client.login(username='chae1234', password='somepassword')
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment=area')
+        self.assertNotIn('Log in and leave a comment', comment_area)
+
+        comment_form = comment_area.find('form', id='commnet-form')
+        self.assertTrue(comment_form.find('textarea', id='id_content'))
+        response = self.client.post(
+            self.post_001.get_absolute_url() + 'new_comment/',
+            {
+                'content': 'comment from chae1234',
+            },
+            follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(Comment.objects.count(), 2)
+        self.assertEqual(self.post_001.comment_set.count(), 2)
+
+        new_comment = Comment.objects.last()
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertIn(new_commnet.post.title, soup.title.text)
+
+        comment_area = soup.find('div', id='comment-area')
+        new_comment_div = comment_area.find('div', id='comment-{}'.format(new_commnet.pk))
+        self.assertIn('chae1234', new_comment_div.text)
+        self.assertIn('comment from chae1234', new_comment_div.text)
+
     def test_tag_page(self):
         response = self.client.get(self.tag_hello.get_absolute_url())
         self.assertEqual(response.status_code, 200)
--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,10 @@ urlpatterns = [
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tags/<str:slug>/', views.tag_page),
-    path('cateogry/<str:slug>/', views.category_page),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/new_comment/', views.new_comment()),
     path('<int:pk>', views.PostDetail.as_view()),
-    path('',views.PostList.as_view()),
+    path('', views.PostList.as_view()),
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index)
 ]
